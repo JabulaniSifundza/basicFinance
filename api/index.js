f8ec1500a4be5431be1f437c4b553c70617712e3
JabulaniSifundza <jjsifundza@gmail.com>
2023-05-01 14:57:21 +0200

    Api routing final

--- api/index.js
+++ api/index.js
@@ -27,6 +27,100 @@ app.post('/api/armortize',  (req, res) => {
       res.status(500).json({error: err})
   }
 })
+
+
+app.post('/api/compound', (req, res)=>{
+  const {interestRate, compoundPeriods, principal, periodsRemaining} = req.body;
+    try{
+        const finance = new Finance();
+        const result = finance.CI(interestRate, compoundPeriods, principal, periodsRemaining);
+        res.status(200).json(result)
+    }
+    catch(err){
+        res.status(500).json({error: err})
+    }
+})
+
+app.post('/api/budget', (req, res)=>{
+  const {totalIncome, totalExpenses, totalSavings, savingsGoal} = req.body;
+  try{
+      const incomeToExpensesRatio =  totalExpenses / totalIncome;
+      const savingsToGoalRatio = Number(totalSavings) / Number(savingsGoal);
+      const surplusDeficit = totalIncome - totalExpenses;
+      const surplusDeficitRatio = surplusDeficit / totalIncome;
+      res.status(200).json({incomeToExpensesRatio: incomeToExpensesRatio, savingsToGoalRatio: savingsToGoalRatio, surplusDeficitRatio: surplusDeficitRatio, surplusDeficit: surplusDeficit})
+  }
+  catch(err){
+      res.status(500).json({error: err})
+  }
+})
+
+app.post('/api/incomestate', (req, res)=>{
+  const {netProfit, revenue, grossProfit, operatingIncome} = req.body;
+  try{
+      const netMarg = ((Number(netProfit) / Number(revenue)) * 100).toFixed(2);
+      const grossMarg = ((Number(grossProfit) / Number(revenue)) * 100).toFixed(2)
+      const operatingMarg = ((Number(operatingIncome) / Number(revenue)) * 100).toFixed(2)
+      res.status(200).json({netMarg: netMarg, grossMarg: grossMarg, operatingMarg: operatingMarg})
+  }
+  catch(err){
+      res.status(500).json({error: err})
+  }
+})
+
+app.post('/api/cagr', (req, res)=>{
+  const {initial, final, periods} = req.body;
+  try{
+      const finance = new Finance();
+      const result = finance.CAGR(initial, final, periods);
+      res.status(200).json(result)
+  }
+  catch(err){
+      res.status(500).json({error: err})
+  }
+})
+
+app.post('/api/roi', (req, res)=>{
+  const {initial, final} = req.body;
+    try{
+        const finance = new Finance();
+        const result = finance.ROI(initial, final);
+        res.status(200).json(result)
+    }
+    catch(err){
+        res.status(500).json({error: err})
+    }
+})
+
+app.post('/api/df', (req, res)=>{
+  const {rate, periods} = req.body;
+  try{
+      const finance = new Finance();
+      const result = finance.DF(rate, periods);
+      res.status(200).json(result)
+  }
+  catch(err){
+      res.status(500).json({error: err})
+  }
+})
+
+app.post('/api/profile', (req, res)=>{
+  const {ticker} = req.body;
+  try{
+      yahooFinance.quote({
+          symbol: ticker,
+          modules: ['financialData', 'summaryDetail']
+      }).then(data => {
+          res.status(200).json(data)
+      }).catch(err => {
+          console.log(err)
+          res.status(500).json({error: err})
+      })
+  }
+  catch(err){
+      res.status(500).json({error: err})
+  }
+})
 // Start the server
 app.listen(3000, () => {
   console.log('Server started on port 3000');
