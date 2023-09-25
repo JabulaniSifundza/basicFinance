f3165b02c800344ea48278de44e1397ef8d100ed
JabulaniSifundza <jjsifundza@gmail.com>
2023-09-25 18:52:28 +0200

    New API

--- api/index.js
+++ api/index.js
@@ -1690,6 +1690,150 @@ app.post('/api/catererBlast', async(req, res)=>{
 	}
 
 })
+
+app.post('/api/webook', (req, res)=>{
+    if (req.body.object) {
+    if (
+      req.body.entry &&
+      req.body.entry[0].changes &&
+      req.body.entry[0].changes[0] &&
+      req.body.entry[0].changes[0].value.messages &&
+      req.body.entry[0].changes[0].value.messages[0]
+    ) {
+      let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
+      let from = req.body.entry[0].changes[0].value.messages[0].from;
+      let text = req.body.entry[0].changes[0].value.messages[0].text?.body;
+      let btn_msg = req.body.entry[0].changes[0].value.messages[0].interactive?.button_reply?.id;
+      let message;
+      if (text.toLowerCase() === 'start') {
+        message = {
+          messaging_product: "whatsapp",
+          to: from,
+          type: "interactive",
+          interactive: {
+            type: "button",
+            body: {
+              text: "What can we do for you today?"
+            },
+            action: {
+              buttons: [
+                {
+                  type: "reply",
+                  reply: {
+                    id: "start_get_help",
+                    title: "Get Help"
+                  }
+                },
+                {
+                  type: "reply",
+                  reply: {
+                    id: "start_data_bundles",
+                    title: "Data Bundles"
+                  }
+                }
+              ]
+            }
+          }
+        }
+      } else if (btn_msg) {
+        switch (btn_msg) {
+          case 'start_get_help':
+            message = {
+              messaging_product: "whatsapp",
+              to: from,
+              text: { body: "You selected 'Get Help'. How can we assist you further?" },
+            };
+            break;
+          case 'start_data_bundles':
+            message = {
+              messaging_product: "whatsapp",
+              to: from,
+              text: { body: "You selected 'Data Bundles'. Here are our available data bundles..." },
+            };
+            break;
+          // Add more cases as needed
+          default:
+            message = {
+              messaging_product: "whatsapp",
+              to: from,
+              text: { body: "I'm sorry, I didn't understand that. Could you please rephrase or select one of the options?" },
+            };
+            break;
+        }
+      } else {
+        message = {
+          messaging_product: "whatsapp",
+          to: from,
+          text: { body: "I'm sorry, I didn't understand that. Could you please rephrase or select one of the options?" },
+        };
+      }
+
+      axios({
+        method: "POST",
+        url: `https://graph.facebook.com/v17.0/134908786370593/messages`,
+        data: message,
+        headers: { 
+          "Content-Type": "application/json",
+          "Authorization": "Bearer EAATUi9MntfEBOZChcSO3AVpYlqXKd5LiwWNYZCZC7UahGUzY6KZBawk3Rgrt7egBQmQIqHj2KjUWF6YbjsRjpHH1wAsjUAtOBiOuN2ZBcjcJJLro1lVDNjjwGIR6EkbJz8NAi4e8AYXA5q2Dz1sEpEZAyDfHs11ZA2Hwf60qyEX4VEQmCge6fhL6htLPZAq9ZCMnYPkdHZBwgjF9ZCbZBCPzSZB0ZD",
+          "Accept-Encoding": "gzip, deflate, br"
+        },
+      }).catch((error) => {
+        console.error('Error occurred:', error);
+      });
+    }
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(404);
+  }
+})
+
+app.get("/api/webhook", (req, res) => {
+  const verify_token = process.env.VERIFY_TOKEN;
+  // Parse params from the webhook verification request
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+  // Check if a token and mode were sent
+  if (mode && token) {
+    // Check the mode and token sent are correct
+    if (mode === "subscribe" && token === verify_token) {
+      // Respond with 200 OK and challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
+app.get("/api/webhook", (req, res) => {
+  /**
+   * UPDATE YOUR VERIFY TOKEN
+   *This will be the Verify Token value when you set up webhook
+  **/
+  const verify_token = "HAPPY";
+
+  // Parse params from the webhook verification request
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode were sent
+  if (mode && token) {
+    // Check the mode and token sent are correct
+    if (mode === "subscribe" && token === verify_token) {
+      // Respond with 200 OK and challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      // Responds with '403 Forbidden' if verify tokens do not match
+      res.sendStatus(403);
+    }
+  }
+});
+
+
 // Start the server
 app.listen(3000, () => {
   console.log('Server started on port 3000');
